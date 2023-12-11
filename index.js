28bdfa2f3be9bee4865c7c6785c02d654c38e8ed
TheoMa76 <tmaerten@normandiewebschool.fr>
2023-12-11 11:44:18 +0100

    update

--- index.js
+++ index.js
@@ -1,20 +1,40 @@
 import express from "express";
 import { simpleRandom } from "./src/utils/simpleRandom.js";
+import { pokemons } from "./src/data/pokemons.js";
+import 'dotenv/config';
+
+const port = process.env.PORT;
 
 const app = express();
-const port = 3001;
 
 app.get("/", (req, res) => {
   res.send("Accueil");
 });
 
 app.get("/random", async (req, res) => {
-  const randomNumber = await simpleRandom();
+    try{
+    const randomNumber = await simpleRandom();
+    res.json({
+        SimpleRandomNumber: randomNumber,
+        });
+    } catch (error) {
+        res.status(500).send("Erreur 500");
+    }
+});
 
-  res.responseType = "json";
-  res.json({
-    SimpleRandomNumber: randomNumber,
-    });
+app.get("/pokemon/:id", (req, res) => {
+    try{
+        const id = req.params.id;
+        const pokemon = pokemons.find((pokemon) => pokemon.id === parseInt(id));
+        if(pokemon === undefined){
+            throw new Error("Pokemon non trouvé");
+        }
+        res.json({Nom :pokemon["name"], Type :pokemon["types"]});
+        
+    } catch (error) {
+        res.status(404).send(error.message);
+    }
+    
 });
 
 app.listen(port, () => {
