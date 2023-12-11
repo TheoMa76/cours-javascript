bdadb93ab83e84a83beff36898747ef58f7a3ccb
TheoMa76 <tmaerten@normandiewebschool.fr>
2023-12-11 10:36:39 +0100

    json simpleRandom

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 import express from "express";
+import { simpleRandom } from "./src/utils/simpleRandom.js";
 
 const app = express();
 const port = 3001;
 
-app.listen(port, () => {
-  console.log(`Server is listening at http://localhost:${port}`);
-});
-
 app.get("/", (req, res) => {
   res.send("Accueil");
 });
+
+app.get("/random", async (req, res) => {
+  const randomNumber = await simpleRandom();
+
+  res.responseType = "json";
+  res.json({
+    SimpleRandomNumber: randomNumber,
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server is listening at http://localhost:${port}`);
+  });
---
+++ src/utils/simpleRandom.js
@@ -0,0 +1,1 @@
+export const simpleRandom = async() => Math.round(Math.random() * 100);
