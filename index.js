60f444c0c7e64213b00392e81f52de6f702af1ea
TheoMa76 <tmaerten@normandiewebschool.fr>
2023-12-11 10:02:10 +0100

    commande

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("hello world !")
+import express from "express";
+
+const app = express();
+const port = 3001;
+
+app.listen(port, () => {
+  console.log(`Server is listening at http://localhost:${port}`);
+});
+
+app.get("/", (req, res) => {
+  res.send("Accueil");
+});
